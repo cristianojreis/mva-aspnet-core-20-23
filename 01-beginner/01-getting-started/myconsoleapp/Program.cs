eb8ee3accdc455d42ce4ba435930587ca3bf0766
crisjreis <cristiano@davero.com.br>
2018-02-10 16:25:46 -0200

    Ajuste finais.

--- 01-beginner/01-getting-started/myconsoleapp/Program.cs
+++ 01-beginner/01-getting-started/myconsoleapp/Program.cs
@@ -6,9 +6,12 @@ namespace myconsoleapp
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Ola Cristiano Jose dos Reis!");
+            Console.Write("Informe seu nome: ");
+            var _nome = Console.ReadLine();
+            Console.WriteLine("\r\nSeja bem vindo {0}.\r\n\r\nIremos somar os valores 1,5 com 2,1 com 1,4 utilizando alguns recursos do C#.\r\n", _nome);
             Console.WriteLine("Resultado da soma: {0}", Somar(1.5d, 2.1d, 1.4d));
+            Console.Write("Pressione qualquer tecla para encerrar a aplicação.");
+            Console.ReadKey();
         }
 
         protected static double Somar(params double[] termos) {
