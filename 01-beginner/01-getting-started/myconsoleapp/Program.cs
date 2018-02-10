e5eeb40bc89610667b09f0a59e2e18439faecaf0
crisjreis <cristiano@davero.com.br>
2018-02-10 16:17:15 -0200

    Primeiro exercicio criando um aplicativo console com dotnet core usando apenas o cli e o VS Code.

---
+++ 01-beginner/01-getting-started/myconsoleapp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace myconsoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ola Cristiano Jose dos Reis!");
+            Console.WriteLine("Resultado da soma: {0}", Somar(1.5d, 2.1d, 1.4d));
+        }
+
+        protected static double Somar(params double[] termos) {
+            double _resultado = 0;
+            foreach (double _termo in termos) {
+                _resultado += _termo;
+            }
+            return _resultado;
+        }
+    }
+}
